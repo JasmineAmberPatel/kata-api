4747627b0201b8516f5ff6c40b16c583d8ffe4a9
Jasmine Amber <jasmineamber.ja@gmail.com>
2019-09-14 16:26:12 +0100

    completed numbers tests

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 
 const express = require('express');
-const { sayHello, uppercase, lowercase, firstCharacter, firstCharacters  } = require('./lib/strings');
+const { sayHello, uppercase, lowercase, firstCharacter, firstCharacters } = require('./lib/strings');
+const { add, subtract, multiply, divide, power, round, roundUp, roundDown, absolute, quotient } = require('./lib/numbers');
 
 const app = express();
 
@@ -16,14 +17,42 @@ app.get('/strings/lower/:string', (req, res) => {
   res.json({ result: lowercase(req.params.string) });
 });
 
-app.get('/strings/first-characters/:string', (req, res) => {
+app.get('/strings/first-character/:string', (req, res) => {
   res.json({ result: firstCharacter(req.params.string) });
 });
 
-app.get('/strings/first-characters/sd32fg45:string', (req, res) => {
+app.get('/strings/first-characters/:string', (req, res) => {
   const string = req.params.string;
   const length = req.query.length;
   res.json({ result: firstCharacters(string, length) });
+});
+
+app.get('/numbers/add/:a/and/:b', (req, res) => {
+  const isNumeric = (string) => {
+    return !Number.isNaN(parseInt(string));
+  };
+  if (isNumeric(req.params.a) && isNumeric(req.params.b)) {
+    const a = parseInt(req.params.a);
+    const b = parseInt(req.params.b);
+
+    res.status(200).json({ result: add(a, b) });
+  } else {
+    res.status(400).json({ error: 'Parameters must be valid numbers.' });
+  }
+});
+
+app.get('/numbers/subtract/{number}/from/{number}', (req, res) => {
+  const isNumeric = (string) => {
+    return !Number.isNaN(parseInt(string));
+  };
+  if (isNumeric(req.params.a) && isNumeric(req.params.b)) {
+    const a = parseInt(req.params.a);
+    const b = parseInt(req.params.b);
+
+    res.status(200).json({ result: add(a, b) });
+  } else {
+    res.status(400).json({ error: 'Parameters must be valid numbers.' });
+  }
 });
 
 module.exports = app;
