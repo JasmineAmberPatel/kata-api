fd66d1e644052b2bfa2ce56f82e7aab21cd2ae7e
Jasmine Amber <jasmineamber.ja@gmail.com>
2019-09-16 11:51:17 +0100

    all divide tests pass

--- src/app.js
+++ src/app.js
@@ -71,17 +71,16 @@ app.post('/numbers/multiply', (req, res) => {
 });
 
 app.post('/numbers/divide', (req, res) => {
-  const zero = [0];
   const isNumeric = (string) => {
     return !Number.isNaN(parseInt(string));
   };
+  if (req.body.b === 0) {
+    res.status(400).json({ error: 'Unable to divide by 0.' });
+  } else
   if (isNumeric(req.body.a) && isNumeric(req.body.b)) {
     res.status(200).json({ result: divide(req.body.a, req.body.b) });
   } else
-  if (((req.body.a) === zero) || ((req.body.b) === zero)) {
-    res.status(400).json({ error: 'Unable to divide by 0.' });
-  } else
-  if (((req.body.a) === undefined) || ((req.body.b) === undefined)) {
+  if ((req.body.a === undefined) || (req.body.b === undefined)) {
     res.status(400).json({ error: 'Parameters "a" and "b" are required.' });
   } else
   if (isNaN(req.body.a) || isNaN(req.body.b)) {
@@ -90,21 +89,20 @@ app.post('/numbers/divide', (req, res) => {
 });
 
 app.post('/numbers/remainder', (req, res) => {
-  const zero = [0];
   const isNumeric = (string) => {
     return !Number.isNaN(parseInt(string));
   };
-  if (isNumeric(req.body.a) && isNumeric(req.body.b)) {
-    res.status(200).json({ result: remainder(req.body.a, req.body.b) });
-  } else
-  if (((req.body.a) === zero) || ((req.body.b) === zero)) {
+  if (req.body.b === 0) {
     res.status(400).json({ error: 'Unable to divide by 0.' });
   } else
-  if (((req.body.a) === undefined) || ((req.body.b) === undefined)) {
+  if ((req.body.a === undefined) || (req.body.b === undefined)) {
     res.status(400).json({ error: 'Parameters "a" and "b" are required.' });
   } else
   if (isNaN(req.body.a) || isNaN(req.body.b)) {
-    res.status(400).json({ error: 'Parameters "a" and "b" must be valid numbers.' });
+    res.status(400).json({ error: 'Parameters must be valid numbers.' });
+  } else
+  if (isNumeric(req.body.a) && isNumeric(req.body.b)) {
+    res.status(200).json({ result: remainder(req.body.a, req.body.b) });
   }
 });
 
